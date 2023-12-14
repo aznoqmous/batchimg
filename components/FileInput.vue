2ced0513ec37f723b9696c5a9058a4f5dbb68482
aznoqmous <paul.landreau931v@gmail.com>
2023-12-14 14:07:36 +0100

    fileinput allow add

--- components/FileInput.vue
+++ components/FileInput.vue
@@ -7,8 +7,13 @@
   const emit = defineEmits(['filesChange'])
     const fileInput = ref(null)
     const files = ref([])
+    let _files = []
     const handleFileInput = (e)=>{
-        files.value = [...e.target.files]
-        emit("filesChange", [...e.target.files])
+        const fileNames = _files.map(f => f.name)
+        let  newFiles = [...e.target.files].filter(f => !fileNames.includes(f.name))
+        console.log(newFiles)
+        _files = [..._files, ...newFiles]
+        files.value = _files
+        emit("filesChange", _files, [...newFiles])
     }
 </script>
